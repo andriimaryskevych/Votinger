c756d9ad5af7b31a645a60f7d05ca2de59a4aa90
Andrii Maryskevych <andrii.maryskevych@gmail.com>
2018-03-20 23:04:10 +0200

    Add project files.

---
+++ Votinger/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Votinger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
